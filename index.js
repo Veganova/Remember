2b864697fa332b35e64c1863b09b2ebf7fbf70a3
Dhruv Sheth <dhruvnsheth@gmail.com>
2018-08-01 14:50:30 -0400

    broken code

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken) => {
-    console.log(accessToken);
-  })
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-// Will see the code in the URL and retrieve user profile
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
-//https://accounts.google.com/o/
-// oauth2/v2/auth?response_type=code&redirect_uri=http
-// %3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=pr
-// ofile%20email&client_id=129497910079-ier91g5l182cakpd25ng4d0
-// 1lgmb8ied.apps.googleusercontent.com
