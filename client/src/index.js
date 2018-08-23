d3aff7b51cb1b962be1f019578d9153e57993841
Dhruv Sheth <dhruvnsheth@gmail.com>
2018-08-23 15:14:47 -0400

    added bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
