6668f2d0fd9a524f2e4f3481c9a6e94a90f6bbbb
Veganova <virajnpatil@gmail.com>
2018-08-29 13:43:57 -0400

    Heroku deployment changes

--- index.js
+++ index.js
@@ -26,5 +26,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/starRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets(files)- main.js, main.css
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
