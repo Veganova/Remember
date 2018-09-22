de8aba6ef5f23669350e5805554d996749ba4c2e
Veganova <virajnpatila@gmail.com>
2018-09-22 16:42:14 -0400

    Using lodash.debounce to update star instead of pressing enter.
    Co-authored-by: Dhruv Sheth <dhruvnsheth@gmail.com>

--- client/src/components/stars/SingleStarView.js
+++ client/src/components/stars/SingleStarView.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
@@ -5,23 +6,28 @@ import '../styles/SingleStarView.css';
 
 class SingleStarView extends Component {
 
-
     constructor(props) {
         super(props);
 
         this.handleNoteEditSubmit = this.handleNoteEditSubmit.bind(this);
         this.handleNoteEditChange = this.handleNoteEditChange.bind(this);
         this.onClickHandler = this.onClickHandler.bind(this);
+        this.updateStar = _.debounce(this.props.updateStar , 500);
+
     }
 
-    handleNoteEditSubmit(event, star) {
+    handleNoteEditSubmit(event) {
         event.preventDefault();
-        this.props.updateStar(star.id, {"data": star.data});
     }
 
 
-    handleNoteEditChange(event) {
-        this.props.updateLocalStar(this.props.star, event.target.value)
+    handleNoteEditChange(event, star) {
+        const data = event.target.value;
+
+        // Need to update locally. Fuse seems to break when this.state is used in the input instead of this.props.
+      // Unsure why but consitently broke the search switching to state. thus using redux to populate this.props.star on keytype.
+        this.props.updateLocalStar(star, data);
+        this.updateStar(star.id, { data });
     }
 
     onClickHandler(event) {
@@ -30,15 +36,16 @@ class SingleStarView extends Component {
 
     render() {
         const star = this.props.star;
+
         return (
-            <form onSubmit={(e)=> this.handleNoteEditSubmit(e, star)}>
+            <form onSubmit={ this.handleNoteEditSubmit }>
                 <div className="row">
                     <div className="col-12">
                         <div className="star-group input-group">
                             <div className="input-group-prepend">
                                 <span className="input-group-text"><i className="fa fa-dot-circle-o"/></span>
                             </div>
-                            <input className="form-control star-input" onChange={this.handleNoteEditChange} type="text" value={star.data} />
+                            <input className="form-control star-input" onChange={(e) => this.handleNoteEditChange(e, star) } type="text" value={star.data} />
                             <div className="input-group-append">
                               <span className="input-group-text star-remove">
                                 <i className="fa fa-times" aria-hidden="true" onClick={() => this.props.removeStar(star.id)}/>
